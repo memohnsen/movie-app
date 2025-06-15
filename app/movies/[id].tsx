0ca7d353da95a7f7a19d87906fc59f984dad8adc
Maddisen Mohnsen <memohnsen@gmail.com>
2025-06-15 11:06:11 -0400

    home page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,14 +1,22 @@
-import { useLocalSearchParams } from 'expo-router'
-import React from 'react'
-import { Text, View } from 'react-native'
+import { fetchMovies } from '@/services/api';
+import useFetch from '@/services/useFetch';
+import { useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Details = () => {
-    const { id } = useLocalSearchParams()
+
+const Details = ({overview, title}: Movie) => {
+  const {id} = useLocalSearchParams()
+  
+  const {data: movies, loading: moviesLoading, error: moviesError} = useFetch(() => fetchMovies({query: ""}))
 
   return (
-    <View>
-      <Text>Movie Details: {id}</Text>
-    </View>
+    <SafeAreaView>
+      <Text className='text-black'>Movie Details: {id}</Text>
+      <Text className='text-black'>Movie Name: {title}</Text>
+      <Text className='text-black'>Movie Overview: {overview}</Text>
+    </SafeAreaView>
   )
 }
 
